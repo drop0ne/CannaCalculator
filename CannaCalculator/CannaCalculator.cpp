6038d1adc0e46d9f718a6fd6137d3655c91b8fbb
drop0ne <core_fire@hotmail.com>
2023-09-21 23:39:31 -0500

    update

--- CannaCalculator/CannaCalculator.cpp
+++ CannaCalculator/CannaCalculator.cpp
@@ -2,99 +2,63 @@
 #include <limits>
 #include <Windows.h>
 
-/*
-        //TODO:
-
-        priority 1
-        replace the decemal input for % loss and replace with " Enter Loss: ##% "
-        replace ## with an integer
-        % is displayed in a cout then the curser needs to be manually moved
-
-        low priority 3
-        then run the cin in a loop to detect the first two input numbers and auto complete to the next prompt for user input
-
-        ********
-                
-        Other task:
-
-        resume aplying color logic / medium priority 2
-
-*/
-
-
-class CannaCalculator {
+class ErrorHandler {
 private:
-    double percentage_THCa;
-    double grams_flower;
-    double mg_THC;
-    double customLoss;
-
-    HANDLE hConsole; // Handle to the console output device
-
-    enum Color {
-        DEFAULT = 7,
-        BLACK = 0,
-        DARK_GREEN = 2,
-        GREEN = 2,
-        BRIGHT_GREEN = 10,
-        BLUE = 1,
-        BRIGHT_RED = 12,
-        RED = 4,
-        LIGHT_BLUE = 3,
-        ICE_BLUE = 9,
-        TEAL_BLUE = 11,
-        WHITE = 7,
-        BRIGHT_WHITE = 15,
-        GRAY = 8,
-        PURPLE = 5,
-        YELLOW = 6,
-        HIGHLIGHT_WITH_BLUE_BLUE = 19,
-        HIGHLIGHT_WITH_BLUE_BLUE2 = 25,
-        HIGHLIGHT_BLUE_WHITE = 23,
-        CHECK = 27
-    };
+    bool errorFlag;
 
 public:
-    CannaCalculator() : percentage_THCa(0), grams_flower(0), mg_THC(0), customLoss(0) {
-        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    ErrorHandler() : errorFlag(false) {}
+
+    void setErrorFlag(bool flag) {
+        errorFlag = flag;
     }
 
-    void run() {
-        setTextColor(GREEN);
+    bool getErrorFlag() const {
+        return errorFlag;
+    }
+};
+
+class InputOutputHandler : public ErrorHandler {
+public:
+    void clearInputBuffer() {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+};
+
+class WindowsAPIHandler {
+public:
+    void setTextColor(int color) {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    }
+
+    void setScreenColor(const char* color) {
+        system(color);
+    }
+};
+
+int main() {
+    double percentage_THCa = 0;
+    double grams_flower = 0;
+    double mg_THC = 0;
+    double customLoss = 0;
+
+    InputOutputHandler ioHandler;
+
+    while (true) {
+        ioHandler.setTextColor(2);
         std::cout << "CannaCalculator\n\n";
-        setTextColor(WHITE);
+        ioHandler.setTextColor(7);
 
         std::cout << "First, enter the percentage of THCa in your cannabis flower.\n"
             << "Then, enter the total number of grams of flower you will use to infuse oil or butter.\n\n";
 
-        bool enableLoss = getPercentage();
-        getGrams();
-
-        mg_THC = calculate_mg_THC(percentage_THCa, grams_flower, enableLoss, customLoss); // Calculate total THC in milligrams
-
-        std::cout << "\n" << percentage_THCa << "% THCa converts to "
-            << static_cast<int>(mg_THC) << "mg THC per " << grams_flower << "g of flower.\n\n";
-
-        for (int i = MIN_SERVINGS; i <= MAX_SERVINGS; i += 2) {
-            std::cout << static_cast<int>(calculate_mg_per_serving(i, mg_THC)) << "mg per " << i << " servings\n";
-        }
-
-        std::cout << "\n\n";
-        system("pause");
-    }
-
-private:
-    static constexpr int MAX_SERVINGS = 27;
-    static constexpr int MIN_SERVINGS = 2;
-
-    bool getPercentage() {
-        char response{};
         bool enableLoss = false;
+        char response{};
 
         while (true) {
             std::cout << "Would you like me to account for loss of THC during the infusing process? (y/n): ";
-            //setScreenColor("color 8");  //Set old text to gray
-            setTextColor(GREEN);
+            ioHandler.setTextColor(2);
             if (std::cin >> response) {
                 if (response == 'y' || response == 'Y') {
                     enableLoss = true;
@@ -103,7 +67,28 @@ private:
                     std::cout << "Would you like to use a custom percentage? (y/n): ";
                     std::cin >> response;
                     if (response == 'y' || response == 'Y') {
-                        customLoss = getCustomLoss();
+                        customLoss = 0;
+
+                        while (true) {
+                            std::cout << "Enter Custom Loss as a decimal number. For example, 0.8 is 20%: ";
+                            try {
+                                if (std::cin >> customLoss) {
+                                    if (customLoss >= 0 && customLoss <= 1) {
+                                        break;
+                                    }
+                                    else {
+                                        throw std::out_of_range("Invalid input. Please enter a decimal number between 0 and 1.");
+                                    }
+                                }
+                                else {
+                                    throw std::runtime_error("Invalid input. Please enter a numeric value.");
+                                }
+                            }
+                            catch (const std::exception& e) {
+                                std::cout << e.what() << "\n";
+                                ioHandler.clearInputBuffer();
+                            }
+                        }
                     }
                     break;
                 }
@@ -116,18 +101,19 @@ private:
             }
             else {
                 std::cout << "Invalid input. Please enter 'y' or 'n'.\n";
-                clearInputBuffer();
+                ioHandler.clearInputBuffer();
             }
         }
 
         while (true) {
-            setScreenColor("color 8");  //Set old text to gray
-            setTextColor(WHITE);
+            ioHandler.setTextColor(7);
             std::cout << "\nWhat percentage of THCa is the cannabis flower you are using? ";
-            setTextColor(GREEN);
+            ioHandler.setTextColor(2);
             try {
-                if (std::cin >> percentage_THCa) {
-                    if (percentage_THCa >= 0 && percentage_THCa <= 100) {
+                double tempPercentage;
+                if (std::cin >> tempPercentage) {
+                    if (tempPercentage >= 0 && tempPercentage <= 100) {
+                        percentage_THCa = tempPercentage;
                         break;
                     }
                     else {
@@ -140,19 +126,17 @@ private:
             }
             catch (const std::exception& e) {
                 std::cout << e.what() << "\n";
-                clearInputBuffer();
+                ioHandler.clearInputBuffer();
             }
         }
 
-        return enableLoss;
-    }
-
-    void getGrams() {
         while (true) {
             std::cout << "How many grams of flower are you using? ";
             try {
-                if (std::cin >> grams_flower) {
-                    if (grams_flower > 0) {
+                double tempGrams;
+                if (std::cin >> tempGrams) {
+                    if (tempGrams > 0) {
+                        grams_flower = tempGrams;
                         break;
                     }
                     else {
@@ -165,71 +149,52 @@ private:
             }
             catch (const std::exception& e) {
                 std::cout << e.what() << "\n";
-                clearInputBuffer();
+                ioHandler.clearInputBuffer();
             }
         }
-    }
 
-    double getCustomLoss() {
-        double loss;
-        while (true) {
-            std::cout << "Enter Custom Loss as a decimal number. For example, 0.8 is 20%: ";
-            try {
-                if (std::cin >> loss) {
-                    if (loss >= 0 && loss <= 1) {
-                        break;
-                    }
-                    else {
-                        throw std::out_of_range("Invalid input. Please enter a decimal number between 0 and 1.");
-                    }
-                }
-                else {
-                    throw std::runtime_error("Invalid input. Please enter a numeric value.");
-                }
-            }
-            catch (const std::exception& e) {
-                std::cout << e.what() << "\n";
-                clearInputBuffer();
-            }
-        }
-        return loss;
-    }
+        double percentLoss = 0.8;
 
-    double calculate_mg_THC(double percentage_THCa, double grams_flower, bool enableLoss, double enableCustomLoss) {
-        double percentLoss = 0.8; // Default loss is 20%
-
-        if (enableCustomLoss > 0) {
-            percentLoss = enableCustomLoss;
+        if (customLoss > 0) {
+            percentLoss = customLoss;
         }
 
         if (enableLoss) {
-            return (percentage_THCa * 10) * percentLoss * grams_flower; // Apply loss factor to the calculation
+            mg_THC = (percentage_THCa * 10) * percentLoss * grams_flower;
+        }
+        else {
+            mg_THC = (percentage_THCa * 10) * grams_flower;
         }
 
-        return (percentage_THCa * 10) * grams_flower; // Calculate THC without loss
-    }
+        ioHandler.setTextColor(7);
+        std::cout << "\n" << percentage_THCa << "% THCa converts to "
+            << static_cast<int>(mg_THC) << "mg THC per " << grams_flower << "g of flower.\n\n";
 
-    double calculate_mg_per_serving(double servings, double mg_THC) {
-        return mg_THC / servings; // Calculate THC per serving
-    }
+        for (int i = 2; i <= 27; i += 2) {
+            std::cout << static_cast<int>(mg_THC / i) << "mg per " << i << " servings\n";
+        }
 
-    void setTextColor(int color) {
-        SetConsoleTextAttribute(hConsole, color);
-    }
+        std::cout << "\n\n";
 
-    void setScreenColor(const char* color) {
-        system(color);
-    }
+        while (true) {
+            std::cout << "Do you want to reset or exit the program? (reset/exit): ";
+            std::cin >> response;
 
-    void clearInputBuffer() {
-        std::cin.clear();
-        std::cin.ignore(9223372036854775807, '\n');  //Hard limit of buffer for x64
+            if (response == "reset") {
+                percentage_THCa = 0;
+                grams_flower = 0;
+                mg_THC = 0;
+                customLoss = 0;
+                break;
+            }
+            else if (response == "exit") {
+                return 0;
+            }
+            else {
+                std::cout << "Invalid input. Please enter 'reset' or 'exit'.\n";
+            }
+        }
     }
-};
-
-int main() {
-    CannaCalculator calculator;
-    calculator.run();
 
     return 0;
 }
